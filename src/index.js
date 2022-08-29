027da2849aa356d3c96e6805db5a32d9aabdeda0
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-08-29 17:31:51 +0100

    add new branch

--- src/index.js
+++ src/index.js
@@ -6,5 +6,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
+// https://restcountries.com/v3.1/all
