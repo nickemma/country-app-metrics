1ed21bf6e3100a8c6c225eeca0f0e80fe99ae284
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-08-30 22:03:20 +0100

    add the icon for nav

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { BsFillArrowRightSquareFill } from 'react-icons/bs';
 import { getCountries } from '../redux/countries/countries';
 
 const Countries = () => {
@@ -13,9 +14,12 @@ const Countries = () => {
       {countries.map((country) => (
         <div key={country?.name.common}>
           <h1>{country?.name.common}</h1>
-          <img src={country?.flags.png} alt="flag" />
+          <img src={country?.flags.svg} alt="flag" />
           <p>{country?.capital}</p>
           <p>{country?.subregion}</p>
+          <button type="button">
+            <BsFillArrowRightSquareFill />
+          </button>
         </div>
       ))}
     </>
