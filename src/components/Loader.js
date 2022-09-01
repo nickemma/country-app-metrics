29cd92aa2b6e69541e85c51026aefb118a0c3160
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-09-01 11:53:46 +0100

    add loading animation

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
+
 const Loader = () => (
   <div className="loading">
     <SkeletonTheme baseColor="#00A9D8" highlightColor="#f8f9f9">
